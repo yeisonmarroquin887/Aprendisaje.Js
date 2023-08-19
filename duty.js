89fed88e599b9869e9f54f44f1f46fb3593c4d4f
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-08-18 21:15:10 -0500

    Ejercicios

--- duty.js
+++ duty.js
@@ -1,3 +1,45 @@
-//tienes la funcion buscarlaPalabra() y tienes un string "Hola encuentra a Kakaroto" tu funcion sera buscar la palabra 
+//tienes la funcion buscarlaPalabra() y tienes un string "Hola encuentra a Kakaroto" tu funcion sera buscar la palabra
 //Kakaroto y devolverla en un array ["Kakaroto"] no te olvides de las variables globales Suerte...
 // Escribe Aqui tu magia👌😁
+const str = "Hola encuentra a Kakaroto"
+function buscarlaPalabra(String) {
+     const phrase = String.split(' ')[3]
+     const res = phrase.split(' ')
+     return res
+}
+console.log(buscarlaPalabra(str))
+
+
+
+//Escribe un programa que convierta una temperatura en grados Celsius a Fahrenheit. Pídele al usuario que ingrese
+// una temperatura en grados Celsius y luego muestra la temperatura equivalente en grados Fahrenheit utilizando 
+// la fórmula de conversión: F = (C * 9/5) + 32
+//Ecribe tu magia Aqui...
+
+
+
+
+//Escribe un programa que tome dos nombres como entrada y muestre un mensaje de saludo personalizado.
+// Si uno de los nombres es "Alice" y el otro es "Bob", muestra un mensaje especial de bienvenida.
+//Eacribe tu magia Aqui...
+
+
+
+
+
+
+//Escribe una función que tome una cadena de texto como argumento y devuelva un array que contenga
+//la longitud de cada palabra en la cadena. Por ejemplo, si la cadena es "Hola mundo, esto es genial",
+//el array resultante sería [4, 5, 4, 2, 6].
+//Escribe tu magia Aqui...
+
+
+
+
+
+
+// chicos os doy este poema "Bajo el cielo estrellado, amor eterno,en cada abrazo, siento tu invierno,en cada beso, un universo tierno,amor amor, en cada latido sincero."
+//lo que deben de hacer es buscar si se encuentra la palabra amor y cuantas veces se repite en el texto
+//crea una variable local que dira la palabra amor se repite ... y hay debes colocar el numero de veces que se repite 
+//usa metodos busca el metodo par eso y usa la concatenacion de las Template String
+//Escribe tu magia Aqui...
